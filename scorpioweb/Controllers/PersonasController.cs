d7f53bc31f4b651cc294ae31e8a0fa080743af66
macs04 <macs04@gmail.com>
2022-07-15 14:51:40 -0500

    Se deja escribir colonias que no stan en la BD y NA para vacias

--- scorpioweb/Controllers/PersonasController.cs
+++ scorpioweb/Controllers/PersonasController.cs
@@ -1698,7 +1698,7 @@ namespace scorpioweb.Controllers
             string motivoViaje, string documentaciónSalirPais, string pasaporte, string visa, string familiaresFuera,
             string enfermedad, string especifiqueEnfermedad, string embarazoLactancia, string tiempoEmbarazo, string tratamiento, string discapacidad, string especifiqueDiscapacidad,
             string servicioMedico, string especifiqueServicioMedico, string institucionServicioMedico, string observacionesSalud, string capturista,
-            IFormFile fotografia, string arraySustancias, string arrayFamiliarReferencia, string arrayDomSec, string arrayFamExtranjero, string combobox)
+            IFormFile fotografia, string arraySustancias, string arrayFamiliarReferencia, string arrayDomSec, string arrayFamExtranjero, string inputAutocomplete)
         {
 
             string currentUser = User.Identity.Name;
@@ -1780,41 +1780,24 @@ namespace scorpioweb.Controllers
                 domicilio.Lat = lat;
                 domicilio.Lng = lng;
 
+                domicilio.NombreCf = normaliza(inputAutocomplete);
+
                 List<Zonas> zonasList = new List<Zonas>();
                 zonasList = (from Zonas in _context.Zonas
                              select Zonas).ToList();
 
-                domicilio.NombreCf = "NA";
-                for (int i = 0; i < zonasList.Count; i++)
-                {
-                    if (zonasList[i].Idzonas.ToString() == combobox)
-                    {
-                        domicilio.NombreCf = zonasList[i].Colonia.ToUpper();
-                    }
-                }
-
-                int index = domicilio.NombreCf.IndexOf(@",");
-                string colonia;
-                if (index == -1)
-                {
-                    colonia = domicilio.NombreCf;
-                }
-                else
-                {
-                    colonia = domicilio.NombreCf.Substring(0, index);
-                }
                 domicilio.Zona = "SIN ZONA ASIGNADA";
                 int matches = 0;
                 for (int i = 0; i < zonasList.Count; i++)
                 {
-                    if (zonasList[i].Colonia.ToUpper() == colonia)
+                    if (zonasList[i].Colonia.ToUpper() == domicilio.NombreCf)
                     {
                         matches++;
                     }
                 }
                 for (int i = 0; i < zonasList.Count; i++)
                 {
-                    if (zonasList[i].Colonia.ToUpper() == colonia && (matches <= 1 || zonasList[i].Cp == domicilio.Cp))
+                    if (zonasList[i].Colonia.ToUpper() == domicilio.NombreCf && (matches <= 1 || zonasList[i].Cp == domicilio.Cp))
                     {
                         domicilio.Zona = zonasList[i].Zona.ToUpper();
                     }
@@ -3496,7 +3479,7 @@ namespace scorpioweb.Controllers
             ViewBag.idcuentaDomicilioSecundario = BuscaId(listaNoSi, domicilio.DomcilioSecundario);
 
             ViewBag.listaZona = listaZonas;
-            ViewBag.idZona = BuscaId(listaZonas, domicilio.Zona);
+            ViewBag.zona = BuscaId(listaZonas, domicilio.Zona);
 
             ViewBag.pais = domicilio.Pais;
 
@@ -3530,7 +3513,7 @@ namespace scorpioweb.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditDomicilio(int id, [Bind("IdDomicilio,TipoDomicilio,Calle,No,TipoUbicacion,NombreCf,Pais,Estado,Municipio,Temporalidad,ResidenciaHabitual,Cp,Referencias,Horario,DomcilioSecundario,Observaciones,Zona,Lat,Lng,PersonaIdPersona,Zona")] Domicilio domicilio, string combobox)
+        public async Task<IActionResult> EditDomicilio(int id, [Bind("IdDomicilio,TipoDomicilio,Calle,No,TipoUbicacion,NombreCf,Pais,Estado,Municipio,Temporalidad,ResidenciaHabitual,Cp,Referencias,Horario,DomcilioSecundario,Observaciones,Zona,Lat,Lng,PersonaIdPersona,Zona")] Domicilio domicilio, string inputAutocomplete)
         {
             if (id != domicilio.PersonaIdPersona)
             {
@@ -3546,41 +3529,24 @@ namespace scorpioweb.Controllers
             //domicilio.Lat = domicilio.Lat;
             //domicilio.Lng = domicilio.Lng;
 
+            domicilio.NombreCf = normaliza(inputAutocomplete);
+
             List<Zonas> zonasList = new List<Zonas>();
             zonasList = (from Zonas in _context.Zonas
                          select Zonas).ToList();
-
-            domicilio.NombreCf = "NA";
-            for (int i = 0; i < zonasList.Count; i++)
-            {
-                if (zonasList[i].Idzonas.ToString() == combobox)
-                {
-                    domicilio.NombreCf = zonasList[i].Colonia.ToUpper();
-                }
-            }
-
-            int index = domicilio.NombreCf.IndexOf(@",");
-            string colonia;
-            if(index == -1)
-            {
-                colonia = domicilio.NombreCf;
-            }
-            else
-            { 
-                colonia = domicilio.NombreCf.Substring(0, index);
-            }
+           
             domicilio.Zona = "SIN ZONA ASIGNADA";
             int matches = 0;
             for (int i = 0; i < zonasList.Count; i++)
             {
-                if (zonasList[i].Colonia.ToUpper() == colonia)
+                if (zonasList[i].Colonia.ToUpper() == domicilio.NombreCf)
                 {
                     matches++;
                 }
             }
             for (int i = 0; i < zonasList.Count; i++)
             {
-                if (zonasList[i].Colonia.ToUpper() == colonia && (matches <= 1 || zonasList[i].Cp == domicilio.Cp))
+                if (zonasList[i].Colonia.ToUpper() == domicilio.NombreCf && (matches <= 1 || zonasList[i].Cp == domicilio.Cp))
                 {
                     domicilio.Zona = zonasList[i].Zona.ToUpper();
                 }
