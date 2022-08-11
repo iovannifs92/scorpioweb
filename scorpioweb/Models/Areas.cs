89350a19b30f5b54193fd9f8299e6e86cd01dca4
macs04 <macs04@gmail.com>
2022-08-11 11:19:58 -0500

    Se hace el reporte de oficialia similar al de google sheets

---
+++ scorpioweb/Models/Areas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace scorpioweb.Enums
+{
+    public partial class Areas
+    {
+        public int IdArea { get; set; }
+        public string UserName { get; set; }
+        public string Area { get; set; }
+    }
+}
