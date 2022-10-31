e0dd7ec3065dab340d11bea035dc953b629bd998
rafalunaavila <rafalunaavila@gmail.com>
2022-10-31 11:32:46 -0600

    Agrega reporte Fichatecnica

---
+++ scorpioweb/Models/EstadoMunicipio.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scorpioweb.Models
+{
+    public class EstadoMunicipio
+    {
+        public Estados estadosVM { get; set; }
+        public Municipios municipiosVM { get; set; }
+        public Contactos contactosVM { get; set; }
+    }
+}
