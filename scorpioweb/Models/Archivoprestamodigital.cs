fc1ec4a08b7ef989ab7ecab43b4131356a48467e
rafalunaavila <rafalunaavila@gmail.com>
2023-02-03 15:36:47 -0600

    prestamo digital, cambios de llenado automatico y permisos

---
+++ scorpioweb/Models/Archivoprestamodigital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace scorpioweb.Models
+{
+    public partial class Archivoprestamodigital
+    {
+        public int IdArchivoPrestamoDigital { get; set; }
+        public string Usuario { get; set; }
+        public DateTime? FechaPrestamo { get; set; }
+        public DateTime? FechaCierre { get; set; }
+        public string TiempoConsulta { get; set; }
+        public string UsuarioOtorgaPermiso { get; set; }
+        public int? ArchivoIdArchivo { get; set; }
+    }
+}
