f20fa50400055626be97217f77c3986385c1f7cf
Rafael Luna Avila <rafalunaavila@gmail.com>
2023-06-07 10:22:57 +0200

    se crea libro negro

---
+++ scorpioweb/Models/Libronegro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace scorpioweb.Models
+{
+    public partial class Libronegro
+    {
+        public int Idlibronegro { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        public string Nombre { get; set; }
+        public string Cp { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string F1 { get; set; }
+        public string F2 { get; set; }
+        public string F3 { get; set; }
+        public string F4 { get; set; }
+    }
+}
