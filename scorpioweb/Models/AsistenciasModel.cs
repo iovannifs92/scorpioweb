3b05f74de89b677efb672c0c2d694ef07c34e183
JuanLermous <ladolypandalupillo@gmail.com>
2024-07-03 10:32:12 -0600

    -se crearon metodos en el controlador de reinsercion para poder ver,crear y eliminar terapias de una persona. -se agregaron modelos para poder ver las terapias de una persona (terapiaAsistenciaViewModal) -En la vista FichaCanalizacion, el tiempo total de la terapia se comento para que se calcule desde el metodo en el controlador mediante la fecha de inicio y la fecha de termino. -E n la vista FichaCanalizacion el contenido del archivo js "FichaCanalizacion" se paso de nuevo a la vista y se borro el archivo js. -se crearon las vistas "Terapias" y "ModalAgregarTerapia" para poder ver,crear y eliminar terapias

---
+++ scorpioweb/Models/AsistenciasModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace scorpioweb.Models
+{
+    public class AsistenciasModel
+    {
+        public string IdAsistencia { get; set; }
+        public string FechaAsistencia { get; set; }
+        public string ObservacionesAsistencia { get; set; }
+        public string Asistio { get; set; }
+        public int TerapiaIdTerapia { get; set; }
+    }
+}
